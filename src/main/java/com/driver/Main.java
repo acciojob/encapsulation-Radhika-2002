71ef78cdc946ac72b3c260bebcc6ff0ad2c79e10
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-02-15 01:16:51 +0530

    Updated code with new feature

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
+// Main.java
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        // Task 4: Trying to access private member directly
+        // Error: Compilation error - private member cannot be accessed directly
+        // System.out.println(obj.name);
+
+        // Task 6: Using setter and getter
+        obj.setName("John");
+        System.out.println(obj.getName()); // Output: John
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
+// RWOnly.java
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // Task 5: Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
